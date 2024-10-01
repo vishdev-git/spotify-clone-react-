621b8a214c9dcc86a098bbd2e80d6f869c382e68
Viswanath R <viswa20.2001@gmail.com>
2024-10-02 00:14:22 +0530

    Context added for seekbar and its functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useContext } from "react";
 import Display from "./components/Display";
 import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
+import { PlayerContext } from "./context/PlayerContext"; // Import PlayerContextProvider
 
 const App = () => {
+  const { audioRef,track } = useContext(PlayerContext); // Use PlayerContext here
+
   return (
-    <div>
+   
       <div className="h-screen bg-black">
         <div className="h-[90%] flex">
           <Sidebar />
           <Display />
         </div>
         <Player />
+        <audio ref={audioRef} src={track.file} preload="auto"></audio>
       </div>
-    </div>
+   
   );
 };
 
--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
-import { albumsData, assets } from "../assets/assets";
+import { albumsData, assets, songsData } from "../assets/assets";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
-  const albumData = albumsData[id]
+  const albumData = albumsData[id];
+  const {playWithId} = useContext(PlayerContext);
   
   return (
     <>
@@ -31,6 +34,21 @@ const DisplayAlbum = () => {
         <p className="hidden sm:block">Date Added</p>
         <img className="m-auto w-4" src={assets.clock_icon} alt="" />
       </div>
+      <hr />
+      {
+        songsData.map((item,index)=>(
+            <div onClick={()=>{playWithId(item.id)}} key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer">
+                <p className="text-white">
+                    <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
+                    <img className="inline w-10 mr-5" src={item.image} alt="" />
+                    {item.name}
+                </p>
+                <p className="text-[15px]">{albumData.name}</p>
+                <p className="text-[15px] hidden sm:block">5 days ago</p>
+                <p className="text-[15px] text-center">{item.duration}</p>
+            </div>
+        ))
+      }
     </>
   );
 };
